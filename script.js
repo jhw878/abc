03c88151686784344bf84b3e4cb5f35ca1444b82
jhw878 <hwjeon0613@naver.com>
2024-07-19 16:08:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const images = [
-    'img1.png', 'img2.png', 'img3.png', 'img4.png',
-    'img1.png', 'img2.png', 'img3.png', 'img4.png'
+    'img1.png', 'img2.png', 'img3.png', 'img4.png', 'img5.png', 'img6.png',
+    'img1.png', 'img2.png', 'img3.png', 'img4.png', 'img5.png', 'img6.png'
 ];
 
 let firstCard = null;
